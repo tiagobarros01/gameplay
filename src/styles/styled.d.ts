b04d03f20c5ab440bf18b5e7785a9098ce2bbac0
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-22 12:53:57 -0300

    chore: Adds more props to DefaultTheme

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -2,17 +2,37 @@ import 'styled-components/native';
 
 declare module 'styled-components/native' {
   export interface DefaultTheme {
-    title: string;
+    title: string,
 
     colors: {
+      gradients: {
+        gradient100: string;
+        gradient90: string;
+        gradient80: string;
+        gradient70: string;
+        gradient60: string;
+        gradient50: string;
+        gradient40: string;
+      },
+
       background: string;
 
+      overlay: string;
+
       title: string;
+      highlight: string;
 
       line: string;
+      on: string;
 
       red: string;
+    },
 
-    };
+    fonts: {
+      title700: string;
+      title500: string;
+      text500: string;
+      text400: string;
+    },
   }
 }
--- src/styles/themes/DefaultTheme.ts
+++ src/styles/themes/DefaultTheme.ts
@@ -2,12 +2,33 @@ export default {
   title: 'Default',
 
   colors: {
+    gradients: {
+      gradient100: '#0A1033',
+      gradient90: '#0D133D',
+      gradient80: '#0E1647',
+      gradient70: '#1B2565',
+      gradient60: '#243189',
+      gradient50: '#1D2766',
+      gradient40: '#495BCC',
+    },
+
     background: '#0D133D',
 
+    overlay: 'rgba(0,0,0, 0.7)',
+
     title: '#DDE3F0',
+    highlight: '#ABB1CC',
 
     line: '#991F36',
+    on: '#32BD50',
 
     red: '#E51C44',
+  },
+
+  fonts: {
+    title700: 'Rajdhani_700Bold',
+    title500: 'Rajdhani_500Medium',
+    text500: 'Inter_500Medium',
+    text400: 'Inter_300Regular',
   },
 };
