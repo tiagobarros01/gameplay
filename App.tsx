89e03d1ce1835be2088d867260f0ec2212366635
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-22 12:53:40 -0300

    chore: Adds expo-fonts & uses fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
+/* eslint-disable camelcase */
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import SignIn from './src/screens/SignIn/index';
 import DefaultTheme from './src/styles/themes/DefaultTheme';
 
-const App = (): JSX.Element => (
-  <ThemeProvider theme={DefaultTheme}>
-    <SignIn />
-  </ThemeProvider>
-);
+const App = (): JSX.Element => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <SignIn />
+    </ThemeProvider>
+  );
+};
 
 export default App;
