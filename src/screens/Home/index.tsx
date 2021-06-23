a9078828201aba4bb344399855f35877d78f089b
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-23 02:15:36 -0300

    style: Adds divider between matches

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,9 +5,12 @@ import { FlatList, View } from 'react-native';
 import { Appointment } from '../../components/Appointment';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
+import { Divider } from '../../components/Divider';
 import { ListHeader } from '../../components/ListHeader';
 import { Profile } from '../../components/Profile';
-import { Container, Header, Content } from './style';
+import {
+  styles, Container, Header, Content,
+} from './style';
 
 export default function Home(): JSX.Element {
   const [category, setCategory] = useState<string>('');
@@ -15,6 +18,18 @@ export default function Home(): JSX.Element {
   const appointments = [
     {
       id: '1',
+      guild: {
+        id: '1',
+        name: 'Legends',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 at 20:40h',
+      description: 'is it today that we\'ll reach the challenger without losing any md10 match',
+    },
+    {
+      id: '2',
       guild: {
         id: '1',
         name: 'Legends',
@@ -49,6 +64,9 @@ export default function Home(): JSX.Element {
             renderItem={({ item }) => (
               <Appointment data={item} />
             )}
+            ItemSeparatorComponent={() => <Divider />}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
           />
         </Content>
       </View>
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -1,5 +1,13 @@
+import { StyleSheet } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
+
+export const styles = StyleSheet.create({
+  matches: {
+    marginTop: 24,
+    marginLeft: 24,
+  },
+});
 
 export const Container = styled.View`
   flex: 1;
