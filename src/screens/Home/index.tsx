f0925fa53bda903d9a20452ea83e16c715e670c6
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-23 16:51:16 -0300

    feat: Separates the appointment data in an file

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,39 +8,13 @@ import { CategorySelect } from '../../components/CategorySelect';
 import { Divider } from '../../components/Divider';
 import { ListHeader } from '../../components/ListHeader';
 import { Profile } from '../../components/Profile';
+import { appointments } from '../../utils/appointments';
 import {
   styles, Container, Header, Content,
 } from './style';
 
 export default function Home(): JSX.Element {
   const [category, setCategory] = useState<string>('');
-
-  const appointments = [
-    {
-      id: '1',
-      guild: {
-        id: '1',
-        name: 'Legends',
-        icon: null,
-        owner: true,
-      },
-      category: '1',
-      date: '22/06 at 20:40h',
-      description: 'is it today that we\'ll reach the challenger without losing any md10 match',
-    },
-    {
-      id: '2',
-      guild: {
-        id: '1',
-        name: 'Legends',
-        icon: null,
-        owner: true,
-      },
-      category: '1',
-      date: '22/06 at 20:40h',
-      description: 'is it today that we\'ll reach the challenger without losing any md10 match',
-    },
-  ];
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
---
+++ src/utils/appointments.ts
@@ -0,0 +1,26 @@
+export const appointments = [
+  {
+    id: '1',
+    guild: {
+      id: '1',
+      name: 'Legends',
+      icon: null,
+      owner: true,
+    },
+    category: '1',
+    date: '22/06 at 20:40h',
+    description: 'is it today that we\'ll reach the challenger without losing any md10 match',
+  },
+  {
+    id: '2',
+    guild: {
+      id: '1',
+      name: 'Legends',
+      icon: null,
+      owner: true,
+    },
+    category: '1',
+    date: '22/06 at 20:40h',
+    description: 'is it today that we\'ll reach the challenger without losing any md10 match',
+  },
+];
