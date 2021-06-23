084206a7b5d33f1a129713f92410abf795f3e2a4
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-23 19:03:31 -0300

    style: Creates a style of AppointmentCreate page

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { RectButton } from 'react-native-gesture-handler';
 
 import { Background } from '../../components/Background';
+import { CategorySelect } from '../../components/CategorySelect';
+import { GuildIcon } from '../../components/GuildIcon';
 import { Header } from '../../components/Header';
+import DefaultTheme from '../../styles/themes/DefaultTheme';
 import {
+  Label, Form, Select, Image, SelectBody,
 } from './style';
 
 export function AppointmentCreate(): JSX.Element {
+  const [category, setCategory] = useState<string>('');
+
   return (
     <Background>
       <Header
         title="Schedule match"
       />
+      <Label style={{ marginLeft: 24, marginTop: 36 }}>
+        Category
+      </Label>
+      <CategorySelect
+        hasCheckBox
+        setCategory={setCategory}
+        categorySelected={category}
+      />
+      <Form>
+        <RectButton>
+          <Select>
+            {
+            // <Image />
+              <GuildIcon />
+            }
+            <SelectBody>
+              <Label>
+                Select a server
+              </Label>
+            </SelectBody>
+            <Feather
+              name="chevron-right"
+              color={DefaultTheme.colors.title}
+              size={18}
+            />
+          </Select>
+        </RectButton>
+      </Form>
+
     </Background>
   );
 }
--- src/screens/AppointmentCreate/style.ts
+++ src/screens/AppointmentCreate/style.ts
@@ -1,5 +1,48 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Label = styled.Text`
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.title700};
+
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const Form = styled.View`
+  padding: 0 24px 0 24px;
+
+  margin-top: 32px;
+`;
+
+export const Select = styled.View`
+  width: 100%;
+  height: 68px;
   
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  border-color: ${({ theme }) => theme.colors.gradients.gradient60};
+  border-width: 1px;
+  border-radius: 8px;
+
+  padding-right: 25px;
+
+  overflow: hidden;
+
+`;
+
+export const Image = styled.View`
+  width: 64px;
+  height: 68px;
+
+  background: ${({ theme }) => theme.colors.gradients.gradient60} ; 
+
+  border-width: 1px;
+  border-radius: 8px;
+`;
+
+export const SelectBody = styled.View`
+  flex: 1;
+
+  align-items: center;
 `;
