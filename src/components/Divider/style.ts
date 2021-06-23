8ba4b99b30ca77a89761fee43aa13d43e4257dcc
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-23 16:08:41 -0300

    style: Changes style of divider

--- src/components/Divider/style.ts
+++ src/components/Divider/style.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  width: 80%;
+  width: 78%;
   height: 1px;
 
   background: ${({ theme }) => theme.colors.gradients.gradient50};
 
-  margin: 21px 0 21px 0;
+  margin: 2px 0 31px 0;
 
   align-self: flex-end;
 `;
