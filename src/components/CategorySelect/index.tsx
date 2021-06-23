213d14b3aa970fb3d2b900ee739bd688e858739b
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-23 16:09:02 -0300

    feat: Creates a hasCheckBox prop

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -1,34 +1,37 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
 
 import { categories } from '../../utils/categories';
 import { Category } from '../Category';
-import {
-  Container,
-} from './style';
+import { Container } from './style';
 
 type Props = {
+  hasCheckBox?: boolean;
   categorySelected: string;
   setCategory: (categoryId: string) => void;
-}
+};
 
-export function CategorySelect({ categorySelected, setCategory }: Props): JSX.Element {
+export function CategorySelect({
+  categorySelected,
+  setCategory,
+  hasCheckBox = false,
+}: Props): JSX.Element {
   return (
     <Container
       horizontal
       showsHorizontalScrollIndicator={false}
       contentContainerStyle={{ paddingRight: 40 }}
     >
-      {
-        categories.map((category) => (
-          <Category
-            key={category.id}
-            icon={category.icon}
-            title={category.title}
-            checked={category.id === categorySelected}
-            onPress={() => setCategory(category.id)}
-          />
-        ))
-      }
+      {categories.map((category) => (
+        <Category
+          key={category.id}
+          icon={category.icon}
+          title={category.title}
+          checked={category.id === categorySelected}
+          onPress={() => setCategory(category.id)}
+          hasCheckBox={hasCheckBox}
+        />
+      ))}
     </Container>
   );
 }
