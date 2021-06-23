4fa994018a4c499ddb9d842d39a9dfeb336bdefe
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-23 17:32:17 -0300

    feat: Creates a Member component

---
+++ src/components/Member/index.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import { View } from 'react-native';
+
+import { MembersData } from '../../@types/MembersData';
+import { Avatar } from '../Avatar';
+import {
+  Container, Title, Status, Info,
+} from './style';
+
+type Props = {
+  data: MembersData;
+}
+
+export function Member({ data: { avatar_url, status, username } }: Props): JSX.Element {
+  const isOnline = status === 'online';
+
+  return (
+    <Container>
+      <Avatar
+        urlImage={avatar_url}
+      />
+      <View>
+        <Title>
+          {username}
+        </Title>
+        <Status>
+          <Info>
+            { isOnline ? 'Available' : 'Busy' }
+          </Info>
+        </Status>
+      </View>
+    </Container>
+  );
+}
---
+++ src/components/Member/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  
+`;
+
+export const Title = styled.Text`
+  
+`;
+
+export const Status = styled.View`
+  
+`;
+
+export const Info = styled.Text`
+  
+`;
